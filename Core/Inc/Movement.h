73aef2146bf308f1543f689e10f39cef2ea56b48
Caleb Hall <chall16@byui.edu>
2023-12-09 14:11:26 -0700

    Fine Tuneing

--- Core/Inc/Movement.h
+++ Core/Inc/Movement.h
@@ -3,6 +3,7 @@ void set_speed(float new_speed); //mm/sec
 void move(float x, float y);  //mm
 void home(); //mm/sec
 void wait(int ms);
+void servo_angle_set(float angle);
 void start_extrusion();
 void stop_extrusion();
 void extrude();
